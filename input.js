dbebf36a23768fa2398666af211d9f5a93c55476
yosef515 <50806000+yosef515@users.noreply.github.com>
2019-05-20 11:01:23 +0300

    Add files via upload

---
+++ input.js
@@ -0,0 +1,18 @@
+﻿    function readValue() {
+      var formName = document.getElementById('name-input').value;
+      var output = document.getElementById('output');
+      var sound = " لاتوجد نتائج ";
+      switch(formName.toLowerCase()) {
+        case 'بني سليم':
+          sound=" قبيلة سُلَيْم هي قبيلة قيسية مضرية عدنانية تنسب إلى سُلَيْم بن مَنْصُور بن  بن عكرمة بن خصفة بن قيس عيلان بن مضر بن نزار بن معد بن عدنان ";
+          break;
+        case 'سليم':
+          sound=" الجد سُلَيْم وإليه تنسب قبيلة سليم ";
+          break;
+       
+        case '':
+          sound="";
+          break;
+      }
+   output.innerHTML = sound;
+    }
