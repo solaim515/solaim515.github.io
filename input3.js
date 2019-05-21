150751f40e3797f5150b426056cc3e79ef3dad52
yosef <50806000+solaim515@users.noreply.github.com>
2019-05-21 10:23:01 +0300

    Add files via upload

---
+++ input3.js
@@ -0,0 +1,15 @@
+﻿    function readValue() {
+      var formName = document.getElementById('name-input').value;
+      var output = document.getElementById('output');
+      var sound = " لاتوجد نتائج ";
+      switch(formName.toLowerCase()) {
+         case 'جبل عمدان':
+          sound="جبل عِمْدَان : هو جبل يتميز بالارتفاع لذلك سُمي عِمْدَان لأنَّه منتصب بشكل عمودي ويقع بين محافظة الكامل ومركز الحنو على بُعد 11 كلم شمال غرب المحافظة غرب الطريق العام الذي يربط المحافظة بقرية الدوارة وتسيل مياهه من جهة الشرق لوادي نهوى حيث يلاقي وادي ساية غرب قرية الشعبة ومن جهة الغرب تسيل لوادي الحنو ";
+          break;
+        
+        case '':
+          sound="";
+          break;
+      }
+   output.innerHTML = sound;
+    }
