b1b4e7876ce1b072059941557624736a2e070b66
yosef <50806000+solaim515@users.noreply.github.com>
2019-06-11 01:16:46 +0300

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,102 @@
+var wa = [
+  {
+    name: "5544",
+    type: "هجرة قبيلة بني سُليم",
+    special_move: "عبدالعزيز السلمي بن عضيب",
+    gif: "image.jpg",
+    description: "الحمد لله الذي أحل الحلال وحرم الحرام والصلاة والسلام على سيد العرب والعجم محمد أفضل المرسلين وأكرم وعلى آله وصحبه وحزبه وكل من قام على نسبه الصحيح سلم أما بعد في سنة ألف ومائتين وثمانين قد طلب مني الأخ عباس بن عبدالله في حضرة الأشياخ على بن خليفان بن على الحوسني والأخ الفقيه محمد بن عبدالله الخوالدي وعلى بن خلود بن عيسى الدوحاني على أن أحرر ممكتوبا في بيان نسبه وسيرة أوائله ليكون هداية لأولاده وتعصيبا بعضا بهم فأجبته لذلك فأقول وأنا العبد الفقير الى الله القوي ، أحمد بن محمد آل با علوي أن أذكر ما علمته وما ذكر لي سابقا وما ذكره لي الأخ المذكور عند هذا التحرير أن عباسا هو إبن عبدالله إبن سليمان بن صابر بن سليمان بن عبدالله الرامي سمي بذلك لأنه ما شوهد أنه أخطأ في رميه العدوان إن رمى بسلاح أو يد ..... فأشتهر بذلك بن شداد بن عامر بن عبدالله بن صابر الصابري وهم رجال بنو صابر الصابريون المضريون وكانوا بنو صابر وبني سليم شعبة في قبائل أرض تهامة بعد مجيء أوائلهم من أرض المغرب والسبب في وصولهم لناحية الباطنة فتنة وقعت بين الجماعة بني صابر على لطم رجل وأتوهم الجماعة بنو سليم لكفاف الفتنة فما نصحوا في مجيئهم بل إفترقوا لكفاف الفتنة فما نصحوا في مجيئهم بل إفترقوا بنوسليم منهم من قام مع المضروب لحق الجوار ولاهم جماعة ومنهم من قام مع الضارب ولهم في ذلك مأرب ولا زال الأمر كذلك زمانا بين صلح وحرب فلما رأوا أن الأمر في زيادة وفتنة مستمرة ظعنت قبيلة من بني صابر وقبيلة من بني سليم لقلة رجالهم وحرارة أكبادهم ومحل قطع دوابهم وكانوا إذ ذاك أهل خيل وإبل وغنم لكن المقاومون لهم أشد منهم بأسا رجالا ومالا فبذلك وصلوا أراضي نجد ثم الى مساكن قوم من العرب يقال لهم آل عريعر ما بين البصرة والإحسا ولم أعرف الموضع لكن ذكر لي الوالد لي هم أقرب للإحساء ومكثوا هناك مدة من الزمان فما صلح لهم ذلك المكان فظعنوا عنه إما الجماعة من بنو سليم أرادوا الإحساء والهجير من أرض نجد والله أعلم بهم وأكثرهم مع بنو صابر خرجوا الى عمان الى أن وصلوا جبل الحفيت فوجدوا سكان الحفيت بنو تميم فأرادوا النزول معهم فأبوا بني تميم عن ذلك إلا بشروط منها أن لا ينزلوا جميعا في موضع واحد بل هم متفرقون في أطراف البلد وأن لا يصدروا للماء أكثر من رحلين إلا بعد الإذن وأن لا راي لهم في الأشياء التي تصدر ......... وأن تكون بينهم وثيقة مكتوبة على هذه الشروط فأبوا عن ذلك بل قالوا لو كنا لذل أهلا ما تركنا التهامة وعشبها الأحلا فتراجعوا بينهم وقالوا إن ظعنا جميعا ما قبلنا أحد في بلدة لكثرتنا فالرأي المصاب نفترق أول الأمر والحال واحد فظعنوا بني سليم وبعض بنو صابر الى أراضي الجبل الأخضر ، وظعنوا بنو صابر الأكثرون الى نحو المشرق الى أن وصلوا أرض الباطنة ونزلوا بها ، وأولهم عمَروا سيح العوينات وأواخرهم وآخرهم عمروا ساحلها ولا زالوا هناك ينقرضون جيلا بعد جيل بسبب الحروب وموت على الفراش المحبوب وأواخرهم وآخرهم إفترقوا على طول الباطنة الى هنا ثبت النسب على الإتفاق بالمقال مع الأخ المذكور في بيان ما علمته إنتهى ثم الذي إنفرد به الأخ المذكور بعد قولنا إفترقوا على طول الباطنة قال وواحد أقرب منا نسبا في الشمال ويسكن أطراف جبال الحتى يقال له صابر بن بطي بن عبدالله بن محمد بن علي بن عبدالله الرامي كذا ذكره الوالد عن أبيه في سيرة نسبه عن جده والذي إنفرد به أيضا ذكره لعدد أجداده ما فوق الثلاثة الى التسعة وقال لا زال الوالد عبدالله يقول إذا مررنا في صحبة الوالد سليمان بأرض العوينات يصف لنا الرموم التي يعرفها هذا الذي بلغنا عن الأجداد على ألسنة الآباء والله أعلم وذلك غشية الأشياخ المذكورين في صدر المكتوب وعلى راي ومشورة منهم بهذا السند أول التحرير وطلب مني الأخ المذكور كذلك إثباته فأقول إن هذا النسب ثابت وصحيح عندي ، وحرم الطعن فيه لقوله جل وعلا (ما آتكم الرسول فخذوه وما نهاكم عنه فإنتهوا) ومما جاء به الصادق الأمين صلى الله وسلم عليه وآله أجمعين : المؤمن مؤتمن على نسبه كما هو مؤتمن على دينه فوجب الإتباع بذلك لإجماع المسلمين ولقولهم من شروط الإسلام أن المسلم إذا تكلم صدق وإذا كلم صدق والإجماع حجة من أقوى الحجج بعد النصوص ولقول الفقهاء الإقرار حجة قاصرة على القروء لم نتعدا لا غيره في الأنساب والَرًضاع والقتل والحرية والطلاق والكفر والإسلام الى آخر عباراتهم كذا في الأشباه وغيرها من المطولات والأصول الى غير ذلك من الدليل والله يهدي من يشاء الى صراط مستقيم وصلى الله على سيدنا محمد وعلى آله وصحبه وسلم إنتهى بعام 12 رجب الأحد سنة 1280 هجريه وحرره العبد الضعيف الى الله القوي أحمد بن محمد آل با علوي نزيل البلد الحسينقين إنتهى نقلا من نسخته كما ذكرنا ذلك أولا بصدر الكاغد وكتبه العبد الضعيف زاهر بم محمد بن سعيد بن أحمد بن سالم بن حمد بن سالم بن قاسم بن أحمد الإسماعيلي الحميري بيده وصلى الله عليه وسلم وعلى صحبه أجمعين صحيح وثابت تاحري في هذه القرطاسة والإقرار بالنسب كتبه العبدالله حمد بن هلال بن حمد السعيدي بيده شهدت بذلك ، وأنا سعيد بن سيف بن على السعدي بينت صحيح وثابت بما قرر في هذه القرطاسية والإقرار بالنسب كتبه الفقير لله علي",
+    
+  },
+  {
+    name: "Charmander",
+    type: "Fire",
+    special_move: "Flamethrower",
+    pic: "https://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png",
+    description: "The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely. The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is happy, and blazes when it is enraged.",
+    gif: 'https://media1.tenor.com/images/e4ad735b0bf29e75999c4c4fae140c89/tenor.gif?itemid=5827908'
+  },
+  {
+    name: "Squirtle",
+    type: "Water",
+    special_move: "Bubble Beam",
+    pic: "https://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png",
+    description: "It evolves into Wartortle starting at level 16, which evolves into Blastoise starting at level 36. Along with Bulbasaur and Charmander, Squirtle is one of three starter Pokémon of Kanto available at the beginning of Pokémon Red, Green, Blue, FireRed, and LeafGreen.",
+    gif: 'https://78.media.tumblr.com/7b6894ba5c9104935321007303332357/tumblr_mgy0dySE5L1rfk2ioo1_500.gif'
+  },
+  {
+    name: "Pikachu",
+    type: "Electric",
+    special_move: "Thunder Bolt",
+    pic: "https://cdn.bulbagarden.net/upload/thumb/0/0d/025Pikachu.png/250px-025Pikachu.png",
+    description: "Pikachu, the Mouse Pokémon, and the evolved form of Pichu. Pikachu's tail is sometimes struck by lightning as it raises it to check its surroundings. Pikachu, the Mouse Pokémon, and the evolved form of Pichu. Pikachu can help other Pikachu who are feeling weak by sharing its electric current.",
+    gif: "https://media1.tenor.com/images/9f7e36b264992626b6791eb67ab47b58/tenor.gif?itemid=5209565"
+  }  
+];
+var index = 0;
+
+function print(message){
+var text = document.getElementById('text');
+  div.innerHTML = message;
+}
+
+function search() {
+ for (i = 0; i < wa.length; i += 1){
+   if (text.value.toLowerCase() === wa[i].name.toLowerCase()) {
+     var name = document.getElementById('name');
+     var image = document.getElementById('image');
+     var description = document.getElementById('description');
+     var gifs = document.getElementById('gifer');
+    
+     name.innerHTML = "";
+     name.innerHTML += '<h1>#' + wa[i].name + '</h1>' + '<img src=' + "'" + wa[i].gif + "'>" +
+       '<br>' + '<p><strong>الوثيقة :</strong> ' + wa[i].type + '</p>' +
+       '<p><strong>الناشر :</strong> ' + wa[i].special_move + '</p>';
+     
+     description.innerHTML = "";
+     description.innerHTML = '<p>' + wa[i].description + '</p>';   
+   }
+ }
+}
+
+$('#text').keypress(function(e){
+ if (e.keyCode === 13){
+   $('#searchButton').click();
+}
+});
+
+function right(current){
+  i = (i + 1) % wa.length;
+     var name = document.getElementById('name');
+     var description = document.getElementById('description');
+     var gifs = document.getElementById('gifer');     
+     name.innerHTML = "";
+     name.innerHTML += '<h1>' + wa[i].name + '</h1>' +
+       '<img src=' + "'" + wa[i].pic + "'>" +
+       '<br>' + '<p><strong>الوثيقة:</strong> ' + wa[i].type + '</p>' +
+       '<p><strong>الناشر:</strong> ' + wa[i].special_move + '</p>';
+     
+     description.innerHTML = "";
+     description.innerHTML = '<p>' + wa[i].description + '</p>';
+       gifs.innerHTML = "";
+     gifs.innerHTML = '<img src=' + "'" + wa[i].gif + "'>";
+     gifs.style.width = '140%';
+}
+
+function left(current){
+  i = (i + -1) % wa.length;
+if(i<0){i=3;}
+     var name = document.getElementById('name');
+     var description = document.getElementById('description');
+     var gifs = document.getElementById('gifer');     
+     name.innerHTML = "";
+     name.innerHTML += '<h1>' + wa[i].name + '</h1>' +
+       '<img src=' + "'" + wa[i].pic + "'>" +
+       '<br>' + '<p><strong>الوثيقة:</strong> ' + wa[i].type + '</p>' +
+       '<p><strong>الناشر:</strong> ' + wa[i].special_move + '</p>';
+     
+     description.innerHTML = "";
+     description.innerHTML = '<p>' + wa[i].description + '</p>';
+       gifs.innerHTML = "";
+     gifs.innerHTML = '<img src=' + "'" + wa[i].gif + "'>";
+     gifs.style.width = '140%';
+}
